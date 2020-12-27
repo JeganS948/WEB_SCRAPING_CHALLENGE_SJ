43aa84e9c5e29ea9423223615e839dda6abc7403
JeganS948 <sharanvika.jegatheeswaran@mail.utoronto.ca>
2020-12-27 00:10:19 -0500

    remove cell run lines

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # import dependencies
 import pandas as pd
 from splinter import Browser
@@ -11,62 +5,29 @@ from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
-# In[2]:
-
-
 # Set Executable Path & Initialize Chrome Browser (Windows path)
 executable_path = {"executable_path": "./chromedriver.exe"}
 browser = Browser("chrome", **executable_path)
-
-
-# In[3]:
-
 
 # Set Executable Path & Initialize Chrome Browser (MAC path)
 # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 # browser = Browser("chrome", **executable_path, headless=False)
 
 
-# <b>NASA Mars News<b>
-
-# In[4]:
-
-
+# NASA Mars News
 # Visit the NASA Mars News Site
 url = "https://mars.nasa.gov/news/"
-
-
-# In[5]:
-
-
 browser.visit(url)
-
-
-# In[6]:
-
 
 # Parse Results HTML with BeautifulSoup
 html = browser.html
 soup = bs(html, 'html.parser')
 
-
-# In[7]:
-
-
 # Scrape the Latest News Title
 title_results = soup.find_all('div', class_='content_title')
 
-
-# In[8]:
-
-
 # Search for paragraph text under news titles
 p_results = soup.find_all('div', class_='article_teaser_body')
-
-
-# In[9]:
-
 
 # Scrape the Latest Paragraph Text
 news_title = title_results[0].text
@@ -75,53 +36,24 @@ print(news_title)
 print(news_p)
 
 
-# <b>JPL Mars Space Images - Featured Images<b>
-
-# In[10]:
-
-
+# JPL Mars Space Images - Featured Images
 # Visit the JPL Featured Space Image
 url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-
-
-# In[11]:
-
-
 browser.visit(url)
-
-
-# In[12]:
-
 
 # Click Button with Class Name full_image
 browser.links.find_by_partial_text('FULL IMAGE')
-
-
-# In[13]:
-
 
 # Find "More Info" Button and Click It
 time.sleep(2)
 browser.links.find_by_partial_text('more info')
 
-
-# In[14]:
-
-
 # Parse Results HTML with BeautifulSoup
 html = browser.html
 soup1 = bs(html, "html.parser")
 
-
-# In[18]:
-
-
 # Retrieve Background Image
 ft_image_url =  soup1.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
-
-
-# In[20]:
-
 
 # Website Url 
 image_url = 'https://www.jpl.nasa.gov'
@@ -131,48 +63,22 @@ ft_image_url = image_url + ft_image_url
 ft_image_url
 
 
-# <b>Mars Facts<b>
-
-# In[21]:
-
-
+# Mars Facts
 # Visit the Mars Facts Site Using Pandas to scrape data
 mars_table_df = pd.read_html("https://space-facts.com/mars/")[0]
-
-
-# In[22]:
-
-
 mars_table_df.columns=["Description", "Value"]
 mars_table_df.set_index("Description", inplace=True)
 mars_table_df
-
-
-# In[23]:
-
 
 # Convert table to html
 facts_table = [mars_table_df.to_html(classes='data table table-borderless', index=False, header=False, border=0)]
 facts_table
 
 
-# <b>Mars Hempisphere<b>
-
-# In[24]:
-
-
+# Mars Hempisphere
 # Visit the USGS Astrogeology Site
 url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-
-
-# In[25]:
-
-
 browser.visit(url)
-
-
-# In[26]:
-
 
 # hemisphere titles
 hemisphere_image_urls = []
@@ -197,10 +103,5 @@ for item in range(len(links)):
     
     # Navigate Backwards
     browser.back()
-
-
-# In[27]:
-
-
-hemisphere_image_urls
+    hemisphere_image_urls
 
